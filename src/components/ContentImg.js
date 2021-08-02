9222562a0c128f863d27a90fcce8c77fa3b6e0ce
MatheusBacochina <bacochinayt@hotmail.com>
2021-08-02 18:25:11 -0300

    first commit

---
+++ src/components/ContentImg.js
@@ -0,0 +1,11 @@
+import '../styles/content.scss'
+
+export function ContentImg(){
+    return(
+        <>
+        <div className="img-content">
+            <div className="melhor-frangobg"></div>
+        </div>
+        </>
+    );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+import frangologo from '../images/frangofritologo.png'
+
+export const Header = styled.div`
+
+background: linear-gradient(180deg, #FAD662 0%, #E8B921 100%);
+display: flex;
+justify-content: center;
+
+
+`
+
+
+export const NavBar = styled.div`
+
+width: 100%;
+max-width: 1100px;
+height: 70px;
+display: flex;
+justify-content: center;
+align-items: center;
+
+
+`
+    
+export const LogoNavBar = styled.div`
+
+background: url({LogoImg});
+width: 200px;
+height: 200px;
+background-repeat: no-repeat;
+background-size: cover;
+
+
+`
+
+
+
